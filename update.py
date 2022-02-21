2d2006701321daccce4850af20c3f6c8e676f6ee
CloseCoder <69109969+KarimjonovSodiq@users.noreply.github.com>
2022-02-21 20:55:32 +0500

    Update update.py

--- update.py
+++ update.py
@@ -49,7 +49,7 @@ if UPSTREAM_REPO is not None:
         srun(["rm", "-rf", ".git"])
 
     srun([f"git init -q \
-            && git config --global user.email e.anastayyar@gmail.com \
+            && git config --global user.email e.karimjonov@gmail.com \
             && git config --global user.name mltb \
             && git add . \
             && git commit -sm update -q \
