dd4d1ce2e6c758063ee9d87a3791b3b17cd541db
CloseCoder <69109969+KarimjonovSodiq@users.noreply.github.com>
2022-02-21 21:21:07 +0500

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
+    UPSTREAM_REPO = "https://github.com/KarimjonovSodiq/cc-leech"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
@@ -48,7 +48,7 @@ if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 srun([f"git init -q \
-        && git config --global user.email e.anastayyar@gmail.com \
+        && git config --global user.email e.karimjonovsodiq15@gmail.com \
         && git config --global user.name mltb \
         && git add . \
         && git commit -sm update -q \
